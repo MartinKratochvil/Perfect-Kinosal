dab4363865bca052cacb431ede67ca0a95a5b9f5
Filip Iran <iranf.03@spst.cz>
2022-01-03 17:51:17 +0100

    v.1.17

--- PerfectKinosal/Kinosál.Designer.cs
+++ PerfectKinosal/Kinosál.Designer.cs
@@ -46,10 +46,11 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(663, 396);
             this.Controls.Add(this.buttonLogin);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.MaximizeBox = false;
             this.MinimumSize = new System.Drawing.Size(379, 251);
             this.Name = "Kinosál";
